98e2dec150884cc54d9f92fb295475dea4e84487
Ole Krüger <git@ole.lol>
2023-12-01 19:33:43 +0000

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      frameworks = pkgs.darwin.apple_sdk.frameworks;
+    in {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "unstacked";
+        version = "0.0.0";
+
+        src = pkgs.nix-gitignore.gitignoreSource [] self;
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          openssl.dev
+          libgpg-error
+          gpgme.dev
+          libgit2
+        ];
+      };
+
+      devShell = pkgs.mkShell {
+        name = "dev";
+
+        inputsFrom = [
+          self.packages.${system}.default
+        ];
+
+        packages = with pkgs;
+          [
+            nil
+            alejandra
+            clippy
+            rust-analyzer
+            rustfmt
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            frameworks.SystemConfiguration
+          ];
+      };
+    });
+}
